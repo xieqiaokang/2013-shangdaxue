78fd2b716ec028f4da9f88d5617f4c13b16e5152
xieqk <xieqk1214@qq.com>
2020-11-02 23:29:04 +0800

    update

--- generate.py
+++ generate.py
@@ -1,16 +1,16 @@
 import os
 
 repo = '2013-shangdaxue'
-path = 'imgs/chongqing'
+path = 'imgs/shanghai'
 prefix = '![](https://cdn.jsdelivr.net/gh/xieqiaokang/' + repo + '@master/'
-final_str = '<photos>'
+final_str = '\n{% gallery %}\n'
 
 img_list = os.listdir(path)
 
 for img_name in img_list:
-    img_str = prefix + path + '/' + img_name + ')'
+    img_str = prefix + path + '/' + img_name + ')\n'
     final_str += img_str
 
-final_str += '</photos>'
+final_str += '{% endgallery %}\n'
 
 print(final_str)
